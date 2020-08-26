c30fe0d16fc69d3c27c54c2e2ff82dd29b6c2ffd
Duy Hung Le <le.duy.hung@sun-asterisk.com>
2020-08-26 16:26:55 +0700

    feat: connect to wallet

--- ui/src/components/Plant/index.js
+++ ui/src/components/Plant/index.js
@@ -4,13 +4,13 @@ import './Plant.css';
 import { PLANT_STATUS } from 'constant';
 import Bubbles from 'components/Animation/Bubbles';
 
-export default function Plant(props) {
+export default function Plant({ plant }) {
   return (
     <div className='flexalign'>
-      {!!props.plant && props.plant.plantStatus === PLANT_STATUS.PLANTED ? (
+      {!!plant && plant.plantStatus === PLANT_STATUS.PLANTED ? (
         <div className='plant'>
           <div className='stem'>
-            <img src={props.plant.plantImg} alt='' className='plantImg' />
+            <img src={plant.plantImg} alt='' className='plantImg' />
             <Bubbles />
           </div>
           <div className='pot'>
--- ui/src/components/Top/index.js
+++ ui/src/components/Top/index.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import oxyImg from 'images/oxygen_bubble_big.png';
 import * as actions from 'store/actions';
+import { IconBuilder } from 'icon-sdk-js';
+import { convertHexToDec } from 'helpers';
 
 import './top.css';
 
 function Top() {
-  const state = useSelector((state) => state);
+  const [balanceOxi, setBalanceOxi] = useState(null);
+  const address = useSelector((state) => state.walletAddress);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,18 +17,70 @@ function Top() {
     dispatch(actions.updatePurses);
   }, [dispatch]);
 
+  useEffect(() => {
+    if (address) {
+      dispatch(actions.getBalanceICX(address));
+
+      const methodName = 'balanceOf';
+      const params = {
+        _owner: address,
+      };
+      const callBuilder = new IconBuilder.CallBuilder();
+      const call = callBuilder
+        .to(process.env.REACT_APP_ADDRESS_CONTRACT_OXI)
+        .method(methodName)
+        .params(params)
+        .build();
+      const request = JSON.stringify({
+        jsonrpc: '2.0',
+        method: 'icx_call',
+        params: call,
+        id: 1,
+      });
+
+      window.dispatchEvent(
+        new CustomEvent('ICONEX_RELAY_REQUEST', {
+          detail: {
+            type: 'REQUEST_JSON-RPC',
+            payload: JSON.parse(request),
+          },
+        })
+      );
+    }
+  }, [address, dispatch]);
+
+  const handleSwitchAccount = () => {
+    window.dispatchEvent(
+      new CustomEvent('ICONEX_RELAY_REQUEST', {
+        detail: {
+          type: 'REQUEST_ADDRESS',
+        },
+      })
+    );
+  };
+
+  const eventHandler = (event) => {
+    var type = event.detail.type;
+    var payload = event.detail.payload;
+    switch (type) {
+      case 'RESPONSE_ADDRESS':
+        dispatch(actions.setAddress(payload));
+        break;
+      case 'RESPONSE_JSON-RPC':
+        if (payload.id === 1) {
+          setBalanceOxi(convertHexToDec(payload.result));
+        }
+        break;
+      default:
+    }
+  };
+  window.addEventListener('ICONEX_RELAY_RESPONSE', eventHandler, false);
+
   return (
     <div className='oxy-area p-10px'>
-      <div className='oxy-num'>
+      <div className='oxy-num' onClick={handleSwitchAccount}>
         <img src={oxyImg} className='oxy-img' alt='oxy' />
-        <strong className='number'>
-          {
-            // fetch wallet data
-            state.purses.length !== 0 && state.connected
-              ? `${state.purses[0].extent} ${state.purses[0].issuerPetname}`
-              : 'not connected'
-          }
-        </strong>
+        <strong className='number'>{address ? balanceOxi : 'Not connected, click here!'}</strong>
       </div>
     </div>
   );
